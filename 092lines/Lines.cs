5d80d295e7d406e8ab222ed58d88317a8a3af0de
Lukáš Macek <luk1998@seznam.cz>
2021-11-21 00:31:57 +0100

    HW4 – not generating cubes enabled, crossed spiral added

--- 092lines/Lines.cs
+++ 092lines/Lines.cs
@@ -360,10 +360,10 @@ namespace _092lines
             hei = 600;
 
             // Specific animation params.
-            param = "width=1.0,anti=true,seed=12,a=56.0";
+            param = "width=1.0,anti=true,seed=12,a=56.0,gen=true";
 
             // Tooltip = help.
-            tooltip = "width=<float>, anti=<bool>, seed=<int>, a=<float>...length of the edges of each cube (min=28.0)";
+            tooltip = "width=<float>, anti=<bool>, seed=<int>, a=<float>...length of the edges of each cube (min=28.0, max=196.0), gen=<bool>...try generate cubes?";
         }
 
         private static void MarkPoint(Canvas c, Color colorAfter, Point point, int lineLength)
@@ -663,6 +663,7 @@ namespace _092lines
             bool antialias = true;  // use anti-aliasing?
             int seed = 12;     // random generator seed
             float lineLength = 56;  // edge length of cube
+            bool generateCubes = true; // generate cubes if possible?
 
             Dictionary<string, string> p = Util.ParseKeyValueList(param);
             if (p.Count > 0)
@@ -680,6 +681,9 @@ namespace _092lines
                 // a=<float>
                 if (Util.TryParse(p, "a", ref lineLength))
                     lineLength = (float)MathSupport.Arith.Clamp(lineLength, 28.0, 196.0);
+
+                // gen=<bool>
+                Util.TryParse(p, "gen", ref generateCubes);
             }
 
             // Practical needed lengths:
@@ -705,174 +709,62 @@ namespace _092lines
             c.Clear(backgroundColor);
             c.SetPenWidth(penWidth);
             c.SetColor(cubeColor);
-
-            Cube cube;
-            while (currentY <= actualHeight)
+            
+            if (generateCubes)
             {
-                while (currentX <= actualWidth)
+                Cube cube;
+                while (currentY <= actualHeight)
                 {
-                    cube = DrawCube(c, currentX, currentY, lineLength);
-                    GenerateCubeSlice(c, cube, random, cubeColor);
-                    cubes++;
+                    while (currentX <= actualWidth)
+                    {
+                        cube = DrawCube(c, currentX, currentY, lineLength);
+                        GenerateCubeSlice(c, cube, random, cubeColor);
+                        cubes++;
 
-                    currentX += gap;
+                        currentX += gap;
+                    }
+
+                    currentX = padding;
+                    currentY += gap;
                 }
-
-                currentX = padding;
-                currentY += gap;
-            }
+            }            
 
             if (cubes == 0)
             {
-                // TODO
+                int segmentLength = 1;
+                int mX = 1;
+                int mY = -1;                
+                currentX = c.Width / 2;
+                currentY = c.Height / 2;
+                int endX = currentX;
+                int endY = currentY;
+                bool moveVertically = true;
+
+                while (endX >= 0 && endX <= c.Width && endY >= 0 && endY <= c.Height)
+                {
+                    if (moveVertically)
+                    {
+                        endY = currentY + mY * segmentLength;
+                        c.Line(currentX, currentY, endX, endY);
+                        mY *= -1;
+                        currentY = endY;
+                        moveVertically = false;
+                    }
+                    else
+                    {
+                        endX = currentX + mX * segmentLength;
+                        c.Line(currentX, currentY, endX, endY);
+                        mX *= -1;
+                        currentX = endX;
+                        segmentLength *= 2;
+                        moveVertically = true;
+                    }                    
+                }
+
+                // Cross.
+                c.Line(0, 0, c.Width, c.Height);
+                c.Line(c.Width, 0, 0, c.Height);
             }
-
-            
-
-            /*
-            int wq = c.Width / 4;
-            int hq = c.Height / 4;
-            int wh = wq + wq;
-            int hh = hq + hq;
-            int minh = Math.Min(wh, hh);
-            double t;
-            int i, j;
-            double cx, cy, angle, x, y;
-
-            c.Clear(Color.Black);
-
-            // 1st quadrant - star.
-            c.SetPenWidth(penWidth);
-            c.SetAntiAlias(antialias);
-
-            const int MAX_LINES = 30;
-            for (i = 0, t = 0.0; i < MAX_LINES; i++, t += 1.0 / MAX_LINES)
-            {
-              c.SetColor(Color.FromArgb(i * 255 / MAX_LINES, 255, 255 - i * 255 / MAX_LINES)); // [0,255,255] -> [255,255,0]
-              c.Line(t * wh, 0, wh - t * wh, hh);
-            }
-            for (i = 0, t = 0.0; i < MAX_LINES; i++, t += 1.0 / MAX_LINES)
-            {
-              c.SetColor(Color.FromArgb(255, 255 - i * 255 / MAX_LINES, i * 255 / MAX_LINES)); // [255,255,0] -> [255,0,255]
-              c.Line(0, hh - t * hh, wh, t * hh);
-            }
-
-            // 2nd quadrant - random hatched squares.
-            double size = minh / 10.0;
-            double padding = size * Math.Sqrt(0.5);
-            c.SetColor(Color.LemonChiffon);
-            c.SetPenWidth(1.0f);
-            Random r = (seed == 0) ? new Random() : new Random(seed);
-
-            for (i = 0; i < objects; i++)
-            {
-              do
-                cx = r.NextDouble() * wh;
-              while (cx < padding ||
-                     cx > wh - padding);
-
-              c.SetAntiAlias(cx > wq);
-              cx += wh;
-
-              do
-                cy = r.NextDouble() * hh;
-              while (cy < padding ||
-                     cy > hh - padding);
-
-              angle = r.NextDouble() * Math.PI;
-
-              double dirx = Math.Sin(angle) * size * 0.5;
-              double diry = Math.Cos(angle) * size * 0.5;
-              cx -= dirx - diry;
-              cy -= diry + dirx;
-              double dx = -diry * 2.0 / hatches;
-              double dy = dirx * 2.0 / hatches;
-              double linx = dirx + dirx;
-              double liny = diry + diry;
-
-              for (j = 0; j++ < hatches; cx += dx, cy += dy)
-                c.Line(cx, cy, cx + linx, cy + liny);
-            }
-
-            // 3rd quadrant - random stars.
-            c.SetColor(Color.LightCoral);
-            c.SetPenWidth(penWidth);
-            size = minh / 16.0;
-            padding = size;
-            const int MAX_SIDES = 30;
-            List<PointF> v = new List<PointF>(MAX_SIDES + 1);
-
-            for (i = 0; i < objects; i++)
-            {
-              do
-                cx = r.NextDouble() * wh;
-              while (cx < padding ||
-                     cx > wh - padding);
-
-              c.SetAntiAlias(cx > wq);
-
-              do
-                cy = r.NextDouble() * hh;
-              while (cy < padding ||
-                     cy > hh - padding);
-              cy += hh;
-
-              int sides = r.Next(3, MAX_SIDES);
-              double dAngle = Math.PI * 2.0 / sides;
-
-              v.Clear();
-              angle = 0.0;
-
-              for (j = 0; j++ < sides; angle += dAngle)
-              {
-                double rad = size * (0.1 + 0.9 * r.NextDouble());
-                x = cx + rad * Math.Sin(angle);
-                y = cy + rad * Math.Cos(angle);
-                v.Add(new PointF((float)x, (float)y));
-              }
-              v.Add(v[0]);
-              c.PolyLine(v);
-            }
-
-            // 4th quadrant - Brownian motion.
-            c.SetPenWidth(penWidth);
-            c.SetAntiAlias(true);
-            size = minh / 10.0;
-            padding = size;
-
-            for (i = 0; i < objects; i++)
-            {
-              do
-                x = r.NextDouble() * wh;
-              while (x < padding ||
-                     x > wh - padding);
-
-              do
-                y = r.NextDouble() * hh;
-              while (y < padding ||
-                     y > hh - padding);
-
-              c.SetColor(Color.FromArgb(127 + r.Next(0, 128),
-                                        127 + r.Next(0, 128),
-                                        127 + r.Next(0, 128)));
-
-              for (j = 0; j++ < 1000;)
-              {
-                angle = r.NextDouble() * Math.PI * 2.0;
-                double rad = size * r.NextDouble();
-                cx = x + rad * Math.Sin(angle);
-                cy = y + rad * Math.Cos(angle);
-                if (cx < 0.0 || cx > wh ||
-                    cy < 0.0 || cy > hh)
-                  break;
-
-                c.Line(x + wh, y + hh, cx + wh, cy + hh);
-                x = cx;
-                y = cy;
-                if (r.NextDouble() > prob)
-                  break;
-              }
-              */
         }
     }
 }
